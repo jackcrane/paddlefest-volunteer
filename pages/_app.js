d28103ff372afbea0d6018493523b4a92e1d155b
jackcrane <jack-mac@users.noreply.github.com>
2022-06-22 13:52:36 -0400

    Add simple analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+
+// _app.tsx
+import React from "react";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <Component {...pageProps} />
+      <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
+      <noscript>
+        {/* eslint-disable @next/next/no-img-element */}
+        <img
+          src="https://queue.simpleanalyticscdn.com/noscript.gif"
+          alt=""
+          referrerPolicy="no-referrer-when-downgrade"
+        />
+      </noscript>
+    </React.Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
